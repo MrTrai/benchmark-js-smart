fa00c141870a6cd0d58accf629505a41bb8e88e7
trai tran <ttran96@gatech.edu>
2019-07-30 02:06:49 -0400

    Add new test cases callback_hell and throw_err_in_callback, ddos

--- throw_err_in_callback.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var axios_1 = require("axios");
-axios_1["default"].get("Foo").then(function (result) {
-    throw new Error("This error is in callback!");
-});
